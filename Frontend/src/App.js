7a96a416b0ff7fa1bec0b4ea60caaac961798ba5
mistickesterio <thiagosilvadacruz163@gmail.com>
2022-11-24 11:52:05 -0300

    first commit

---
+++ Frontend/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Jobs from "./Components/Jobs/Jobs";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="Titulo">Quick Jobs</h1>
+      <ul>
+      </ul>
+      <Jobs></Jobs>
+    </div>
+  );
+}
+
+export default App;
